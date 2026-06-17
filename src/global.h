2dd65f4020c2029a8b79127168ef07e8cea3aaf7
chickendude <pokemaster103@gmail.com>
2026-06-16 21:19:16 -0400

    Set up basic battle flow to load characters and display them

---
+++ src/global.h
@@ -0,0 +1,8 @@
+#ifndef LASTLEAVES_GLOBAL_H
+#define LASTLEAVES_GLOBAL_H
+#include <tonc_types.h>
+
+/** Buffer for OAM entries so we can update the OAM outside of VBlank. */
+OBJ_ATTR oam_buf[128];
+
+#endif //LASTLEAVES_GLOBAL_H
