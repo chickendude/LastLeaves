b5b82199071519c3d24e770f7bb20f13917d5ec7
chickendude <pokemaster103@gmail.com>
2026-07-26 13:56:54 -0400

    Increase attack combo array size by one to handle final "ATK_NONE"

--- src/battle_vars.h
+++ src/battle_vars.h
@@ -48,7 +48,7 @@ typedef struct BattleCharacter
     int disp_mp;
     /** Currently displayed stamina value. */
     int disp_sta;
-    AttackDir attack_combo[MAX_COMBO];
+    AttackDir attack_combo[MAX_COMBO + 1];
 } BattleCharacter;
 
 typedef struct DamageText
